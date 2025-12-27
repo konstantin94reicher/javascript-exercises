0da4a3c0a02e713fd3f1ac2781ecb75c95ff9509
konstantin94reicher <205547482+konstantin94reicher@users.noreply.github.com>
2025-12-27 07:10:24 +0100

    started 07_reverseString

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function (arr, num1, num2, num3, num4) {
+  const index = [num1, num2, num3, num4];
+  for (let i of index) {
+    const indexI = arr.indexOf(i);
+    if (arr.includes(i)) {
+      if (indexI > -1) {
+        arr.splice(indexI, 1);
+      }
+    }
+  }
+  return arr;
 };
 
 // Do not edit below this line
