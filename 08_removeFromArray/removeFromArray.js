55772fd1b8e9761745a359eb02e1d8d4c2a5e004
konstantin94reicher <205547482+konstantin94reicher@users.noreply.github.com>
2025-12-29 15:31:11 +0100

    08_remove &  09_sumAll done

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,14 +1,5 @@
-const removeFromArray = function (arr, num1, num2, num3, num4) {
-  const index = [num1, num2, num3, num4];
-  for (let i of index) {
-    const indexI = arr.indexOf(i);
-    if (arr.includes(i)) {
-      if (indexI > -1) {
-        arr.splice(indexI, 1);
-      }
-    }
-  }
-  return arr;
+const removeFromArray = function (arr, ...valuesToRemove) {
+  return arr.filter(item => !valuesToRemove.includes(item)); 
 };
 
 // Do not edit below this line
--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
-
+const sumAll = function (num1, num2) {
+  if (typeof num1 != "number" || typeof num2 != "number") {
+    return "ERROR";
+  } else if (num1 < 0 || num2 < 0) {
+    return "ERROR";
+  } else if (Math.floor(num1) != num1 || Math.floor(num2) != num2) {
+    return "ERROR";
+  } else {
+    let arr = [num1, num2];
+    const arrSorted = arr.sort();
+    let numbers = [];
+    for (let i = arrSorted[0]; i <= arrSorted[1]; i++) {
+      numbers.push(i);
+    }
+    const initialValue = 0;
+    const sumOfArray = numbers.reduce(
+      (acc, curr) => acc + curr,
+      initialValue
+    );
+    return sumOfArray;
+  }
 };
 
 // Do not edit below this line
