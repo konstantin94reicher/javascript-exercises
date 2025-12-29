4f5d8978606a5c5c2f1fd072444c1d075868411b
konstantin94reicher <205547482+konstantin94reicher@users.noreply.github.com>
2025-12-29 16:11:31 +0100

    11_tempConversion done

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,11 +1,44 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (temp) {
+  const fahrenheitToCelcius = (f) => ((f - 32) * 5) / 9;
+  const tempConverted = fahrenheitToCelcius(temp);
+  if (Math.floor(tempConverted) != tempConverted) {
+    const tempConvertedString = tempConverted.toFixed(1);
+    return parseFloat(tempConvertedString);
+  } else {
+    return tempConverted;
+  }
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (temp) {
+  const celciusToFahrenheit = (f) => (f * 9) / 5 + 32;
+  const tempConverted = celciusToFahrenheit(temp);
+  if (Math.floor(tempConverted) != tempConverted) {
+    const tempConvertedString = tempConverted.toFixed(1);
+    return parseFloat(tempConvertedString);
+  } else {
+    return tempConverted;
+  }
 };
+
+
+// vereinte Funktion
+// const convertTemperature = function (temp, fn) {
+//   const tempConverted = fn(temp);
+//   if (Math.floor(tempConverted) != tempConverted) {
+//     const tempConvertedString = tempConverted.toFixed(1);
+//     return parseFloat(tempConvertedString);
+//   } else {
+//     return tempConverted;
+//   }
+// };
+
+// console.log(convertToCelsius(32));
+// console.log(convertToFahrenheit(0));
+// console.log(convertTemperature(32, convertToCelsius));
+// console.log(convertTemperature(0, convertToFahrenheit));
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
