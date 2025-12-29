5df8ee8b9d46d01886868e71187f556dcf7a1995
konstantin94reicher <205547482+konstantin94reicher@users.noreply.github.com>
2025-12-29 15:46:02 +0100

    10_leapYears done

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,5 +1,19 @@
-const leapYears = function() {
+const leapYears = function(num) {
+    // Leap years are years divisible by four (like 1984 and 2004). 
+    // However, years divisible by 100 are not leap years (such as 1800 and 1900) 
+    // unless they are divisible by 400 (like 1600 and 2000, which were in fact leap years). 
+    
+    const divBy4 = num % 4 == 0; 
+    const divBy100 = num % 100 == 0; 
+    const divBy400 = num % 400 == 0; 
 
+    if (divBy4 && !divBy100) {
+        return true; 
+    } else if (divBy400) {
+        return true; 
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
